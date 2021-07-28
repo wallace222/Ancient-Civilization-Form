24fbdd5ff365d13f430a5687ba70bb3662af37ea
wallace222 <ashbyw00business@gmail.com>
2021-07-29 03:49:02 +1200

    Changing The Purpose Of App

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,47 +1,36 @@
-import React from 'react';
-import logo from '../logo.svg';
+import React, {useState} from 'react';
 import './styles/App.css';
 
+
+
+
+
 function App() {
-  return (
+
+const formData = { "MESOPOTAMIA" : 
+"
+
+",
+"" : "",
+"" : "",
+"" : "",
+"" : "",
+"" : "",} 
+
+return (
     <div className="App">
+      {/* basic header right here */}
+
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>client/src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React 17
-        </a>
-        <p>
-          Edit <code>routes/api/crud.js</code> for adding Backend APIs.<br/>
-          View existing APIs at <a className="App-link"
-          href="http://localhost:5000/api/v1/get"
-          target="_blank"
-          rel="noopener noreferrer">/api/v1/...</a>
-        </p>
-        <a 
-          className="App-link"
-          href="https://github.com/GauravWalia19/MernBoilerPlate"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          MERN Boilerplate 1.0.2
-        </a>
-        <br />
-        <a 
-          className="App-link"
-          href="https://www.heroku.com/"
-          target="_blank"
-          rel="noopener noreferrer">
-          Deploy on Heroku
-        </a>
+        <h1> This ain't even a calculator app any more</h1>
       </header>
+
+      <div>
+        This is just for practice mind you don't take yourself so seriously
+        Why don't I just start this one off. By making a form that displays data on it. no backend functionality
+     
+      </div>
+
     </div>
   );
 }
